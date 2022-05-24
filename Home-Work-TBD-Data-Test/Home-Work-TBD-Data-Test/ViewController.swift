93fb2776e3c1d576419a312d3fb764087ef1c36e
Yan Fayzulaev <workmaxp@yandex.ru>
2022-05-24 16:32:27 +0300

    Проект на сервере.

---
+++ Home-Work-TBD-Data-Test/Home-Work-TBD-Data-Test/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Home-Work-TBD-Data-Test
+//
+//  Created by Bandit on 24.05.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
